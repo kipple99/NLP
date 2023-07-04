c966d04e93e6caad1cd538603c4db7d664da9892
Sungwoo Kuk <kuksungwoo99@naver.com>
2023-07-04 14:03:50 +0900

    등장 빈도, 단어 길이 기준의 정제를 이후에 쉽게 사용할 수 있는 함수

---
+++ processing.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+# 등장 빈도 기준 정제 함수
+def clean_by_freq(tokenized_words, cut_off_count):
+    # 파이썬의 Counter 모듈을 통해 단어의 빈도수 카운트하여 단어 집합 생성
+    vocab = Counter(tokenized_words)
+    
+    # 빈도수가 cut_off_count 이하인 단어 set 추출
+    uncommon_words = {key for key, value in vocab.items() if value <= cut_off_count}
+    
+    # uncommon_words에 포함되지 않는 단어 리스트 생성
+    cleaned_words = [word for word in tokenized_words if word not in uncommon_words]
+
+    return cleaned_words
+
+# 단어 길이 기준 정제 함수
+def clean_by_len(tokenized_words, cut_off_length):
+    # 길이가 cut_off_length 이하인 단어 제거
+    cleaned_by_freq_len = []
+    
+    for word in tokenized_words:
+        if len(word) > cut_off_length:
+            cleaned_by_freq_len.append(word)
+
+    return cleaned_by_freq_len
